cad04bf781b77892712513b1d675547fc0c7a52a
jeffbat <75343055+jeffbat@users.noreply.github.com>
2021-07-07 16:31:41 -0400

    today's work

--- app.js
+++ app.js
@@ -1,252 +1,47 @@
 $(()=> {
 
 
-    $('button').on("click", (event)=>{
+  $('button').on("click", (event)=>{
     event.preventDefault();
-
+    const $symbol = $('#symbol');
+    const $buttonValue = $(event.currentTarget).val(); // User clicks a button, then button.value is in the $(event.currentTarget).val();
 
     $.ajax({
-      url: 'https://api.fintel.io/web/v/0.0/so/us/tsla',
+      url: 'https://api.fintel.io/web/v/0.0/so/us/' + $symbol.val(),
       type: "GET",
-      headers: { 'X-API-KEY': 'sk_dcf0afeef2e74f3ba163933283d88fbf',
-      }
-    })
-
-    .then (
-      (incomingData) => {
-      // console.log(incomingData.country)
+      headers: { 'X-API-KEY': 'sk_dcf0afeef2e74f3ba163933283d88fbf'}
+    }).then ((incomingData) => {
+// -- Step 1: Collect data to render to html
       const $container = $('.container');
       let $country = incomingData.country;
       let $exchange = incomingData.exchange;
       let $name = incomingData.name;
       let $owners = incomingData.owners;
-          for(let i = 0; i < $owners.length; i++){
-            $owners[i].name
-          }
+      let ownerNames= '';
+
+      for(let i = 0; i < $owners.length; i++){
+        ownerNames += $owners[i].name + '<br/>';
+      }
+
+// -- Step 2: WRITE TO HTML
       const $div1 = $('<div>');
-      $div1.html(`Country: ${incomingData.country} <br/>
-                  Exchange: ${incomingData.exchange} <br/>
-                  Name: ${incomingData.name} <br/>
-                  owners: ${incomingData.owners} <br/> `)
+      if ($buttonValue === '0') {
+        $div1.html(`Country: ${incomingData.country} <br`);
+      } else if ($buttonValue == '1') {
+        $div1.html(`Exchange: ${incomingData.exchange} <br/>`);
+      } else if ($buttonValue == '2') {
+        $div1.html(`Name: ${incomingData.name} <br/>`);
+      } else if ($buttonValue == '3') {
+
+      } else if ($buttonValue == '4') {
+
+      }
 
       $container.append($div1);
-
-
     },
-
-
     () => {
         console.log('bad request')
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //
-    //
-    //     $.ajax({
-    //       url: 'https://api.fintel.io/web/v/0.0/n/us/tsla',
-    //       type: "GET",
-    //       headers: { 'X-API-KEY': 'sk_dcf0afeef2e74f3ba163933283d88fbf',
-    //     }
-    //     })
-    //
-    //     .then (
-    //       (owners) => {
-    //         console.log(owners)
-    //         const $container = $('.container');
-    //         for (let i = 0; i < owners.length; i++) {
-    //         let object = owners[i];
-    //         const $div1 = $('<div>');
-    //         $div1.html(`Symbol: ${object.symbol} <br/>
-    //           Country: ${object.country} <br/>
-    //           Security Short Volume: ${object.securityShortVolume} <br/>
-    //           Stock Regulatory Filings: ${object.stockRegulatoryFillings} <br/><br/>`);
-    //           $container.append($div1);
-    //
-    //         }
-    //       },
-    //       () => {
-    //         //failure
-    //         console.log('bad request')
-    //       }
-    //     )
-    //
-    //
-    //
-    //     $.ajax({
-    //       url: 'https://api.fintel.io/web/v/0.0/ss/us/tsla',
-    //       type: "GET",
-    //       headers: { 'X-API-KEY': 'sk_dcf0afeef2e74f3ba163933283d88fbf',
-    //       }
-    //     })
-    //
-    //     .then (
-    //       (owners) => {
-    //       console.log(owners)
-    //       const $container = $('.container');
-    //       for (let i = 0; i < owners.length; i++) {
-    //       let object = owners[i];
-    //       const $div1 = $('<div>');
-    //       $div1.html(`Owners: ${object.owners} <br/>
-    //         // Country: ${object.country} <br/>
-    //         // Security Short Volume: ${object.securityShortVolume} <br/>
-    //         // Stock Regulatory Filings: ${object.stockRegulatoryFillings} <br/><br/>`);
-    //       $container.append($div1);
-    //
-    //       }
-    //     },
-    //     () => {
-    //     //failure
-    //     console.log('bad request')
-    //     }
-    //   )
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    // $.ajax({
-    //   url: 'https://api.fintel.io/web/v/0.0/sf/us/tsla',
-    //   type: "GET",
-    //   headers: { 'X-API-KEY': 'sk_dcf0afeef2e74f3ba163933283d88fbf',
-    // }
-    // })
-    //
-    // .then (
-    //   (secFillings) => {
-    //     console.log(secFillings)
-    //     const $container = $('.container');
-    //     for (let i = 0; i < secFillings.length; i++) {
-    //     let object = secFillings[i];
-    //     const $div1 = $('<div>');
-    //       $div1.html(`Symbol: ${object.symbol} <br/>
-    //         Country: ${object.country} <br/>
-    //         Security Short Volume: ${object.securityShortVolume} <br/>
-    //         Stock Regulatory Filings: ${object.stockRegulatoryFillings} <br/><br/>`);
-    //         $container.append($div1);
-    //
-    //     }
-    //   },
-    //   () => {
-    //     //failure
-    //     console.log('bad request')
-    //   }
-    // )
-    //
-    //
-    // $.ajax({
-    //   url: 'https://api.fintel.io/web/v/0.0/i/scion-asset-management-llc',
-    //   type: "GET",
-    //   headers: { 'X-API-KEY': 'sk_dcf0afeef2e74f3ba163933283d88fbf',
-    // }
-    // })
-    //
-    // .then (
-    //   (secFillings) => {
-    //       console.log(secFillings)
-    //       const $container = $('.container');
-    //       for (let i = 0; i < secFillings.length; i++) {
-    //       let object = secFillings[i];
-    //       const $div1 = $('<div>');
-    //       $div1.html(`Symbol: ${object.symbol} <br/>
-    //         Country: ${object.country} <br/>
-    //         Security Short Volume: ${object.securityShortVolume} <br/>
-    //         Stock Regulatory Filings: ${object.stockRegulatoryFillings} <br/><br/>`);
-    //       $container.append($div1);
-    //
-    //     }
-    //   },
-    //   () => {
-    //     //failure
-    //     console.log('bad request')
-    //   }
-    // )
-
-
-
-
-
-
-
-
-
 
   })
 })
