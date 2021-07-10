17baed87099f2507ce24fff6ec73dc78f61b60a2
jeffbat <75343055+jeffbat@users.noreply.github.com>
2021-07-09 23:06:46 -0400

    today's work

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ $(()=> {
 // -- Step 2: WRITE TO HTML
       const $div1 = $('<div>');
       if ($buttonValue === '0') {
-        $div1.html(`Country: ${incomingData.country} <br`);
+        $div1.html(`Country: ${incomingData.country} <br/>`);
       } else if ($buttonValue == '1') {
         $div1.html(`Exchange: ${incomingData.exchange} <br/>`);
       } else if ($buttonValue == '2') {
@@ -47,19 +47,17 @@ $(()=> {
     })
 
   })
-})
-//
-
-$(function() {
-  setInterval(function() {
-    $('#carouseler .carousels').animate({'margin-left': '-=720px'}, 1000)
-  }, 3000)
-  //setInterval
-    //animate margin-left
-    //if it's last slide, go to position 1 (0px);
-
-    //listen for mouseenter and pause
-    //resume on mouseLeave
-
-
 });
+
+
+
+let width = 720;
+let animationSpeed = 1000;
+let pause = 3000;
+
+let $slider = $('#slider');
+let $slideContainer = $slider.find('.slides');
+
+setInterval(function() {
+  $slider.animate({'margin-left': '-='+width}, animationSpeed);
+}, pause);
